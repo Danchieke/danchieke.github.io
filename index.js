a74e05d9b17c87f8f4bd4ea7e37b368d1fb86185
DEkeSparta <deke@spartaglobal.com>
2018-09-03 10:17:59 +0100

    Added running animation, player obj

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 window.onload = function(){
 
+  var gameWindow = document.getElementById("gameWindow");
+  var windowSize = [gameWindow.clientWidth, gameWindow.clientHeight];
+  console.log(windowSize);
+  var player = {
+    image:"images/RunningTransp.gif",
+    x:0,
+    y:windowSize[1] - 203,
+    lanes:[]
+  };
 
+  var playerBox = document.createElement("div");
+  playerBox.className += " gameObj";
+  playerBox.style.backgroundImage = "url('"+player.image+"')";
+  playerBox.style.left = player.x+"px";
+  playerBox.style.top = player.y+"px";
+  playerBox.style.width = "176px";
+  playerBox.style.height = "203px";
+  gameWindow.appendChild(playerBox);
+
+  setInterval(function(){
+    playerBox.style.backgroundImage = "url('"+player.image+"')";
+  },100);
 }
